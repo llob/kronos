56fe09aa805362488a00d74a732db89a66252c1a
Soeren Boll Overgaard <boll@cercare-medical.com>
2021-03-28 09:07:18 +0200

    Initial work on inline documentation

--- abstractmodels/abstractissue.h
+++ abstractmodels/abstractissue.h
@@ -5,6 +5,11 @@
 #include <QString>
 #include "modelobject.h"
 
+/**
+ * @brief The AbstractIssue class encapsulates an issue
+ *  with which worklogs can be associated. An issue may be
+ *  any object that time may be spent on.
+ */
 class AbstractIssue : public ModelObject
 {
 protected:
@@ -13,12 +18,43 @@ protected:
     QString mId;
 public:
     AbstractIssue();
+    /**
+     * @brief setKey Set the unique key identifying this issue
+     *  An issue may be identified either by their id or their key
+     * @param key The key
+     */
     void setKey(const QString key);
+    /**
+     * @brief key Get the unique key identifying this issue
+     * @return The key
+     */
     QString key() const;
+    /**
+     * @brief setId Set the unique id identifying this issue
+     *  An issue may be identified by either its key or its id
+     * @param id The id
+     */
     void setId(const QString id);
+    /**
+     * @brief id Get the unique id identifying this issue
+     * @return
+     */
     QString id() const;
+    /**
+     * @brief setSummary Set the one-line summary of this issue
+     * @param summary
+     */
     void setSummary(const QString summary);
+    /**
+     * @brief summary Get the oneline summary of this issue
+     * @return
+     */
     QString summary() const;
+    /**
+     * @brief toJson Encode this issue as a UTF-8 JSON string
+     *  stored as a byte array
+     * @return
+     */
     QByteArray toJson() const;
 };
 
--- abstractmodels/abstractuser.h
+++ abstractmodels/abstractuser.h
@@ -29,4 +29,6 @@ public:
     void setActive(const bool active);
 };
 
+typedef QSharedPointer<AbstractUser> PAbstractUser;
+
 #endif // ABSTRACTUSER_H
