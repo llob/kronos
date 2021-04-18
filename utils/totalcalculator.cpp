6c277c8645495b387a753d4a82b595a75a7d14df
Soeren Boll Overgaard <boll@cercare-medical.com>
2021-04-18 21:30:25 +0200

    Only update TotalCalculator when authentication state changes to authenticated

--- utils/totalcalculator.cpp
+++ utils/totalcalculator.cpp
@@ -12,8 +12,13 @@ void TotalCalculator::search(QString query)
 TotalCalculator::TotalCalculator() : QObject()
 {
     QObject::connect(&mAuthenticationState, &AuthenticationState::stateChanged,
-                     this, &TotalCalculator::update);
-
+        [this] (AuthenticationState::State oldState, AuthenticationState::State newState, const QString message) {
+            Q_UNUSED(oldState);
+            Q_UNUSED(message);
+            if (newState == AuthenticationState::AUTHENTICATED) {
+                this->update();
+            }
+        });
 }
 
 
