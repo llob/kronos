154a49203c38fe2f201a5e497bb1fae1e727ecd4
Soeren Boll Overgaard <boll@cercare-medical.com>
2021-04-18 22:31:09 +0200

    Clean up use of recent issues

--- utils/recentissues.cpp
+++ utils/recentissues.cpp
@@ -9,8 +9,19 @@ RecentIssues::RecentIssues() : QObject()
 
 void RecentIssues::addIssue(QSharedPointer<AbstractIssue> issue)
 {
-    mIssues.removeAll(issue);
+    // Ensure that we don't have duplicates
+    QList<int> indices;
+    for (int i = 0; i < mIssues.length(); i++) {
+        if (issue->id() == mIssues.at(i)->id()) {
+            indices.append(i);
+        }
+    }
+    foreach (int index, indices) {
+        mIssues.removeAt(index);
+    }
+    // Add the issue
     mIssues.push_front(issue);
+    // Trim list of issues to configured max number of recent issues
     if (mIssues.length() > mMaxIssueCount) {
         mIssues.removeLast();
     }
